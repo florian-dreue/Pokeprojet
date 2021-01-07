ed7aa7d1f2814b668de91ee6049fcb5b0e243acf
florian-dreue <73182533+florian-dreue@users.noreply.github.com>
2021-01-07 11:21:28 +0100

    Update view.php
    Modification esthétique de la vue

--- templates/Pokemons/view.php
+++ templates/Pokemons/view.php
@@ -8,24 +8,39 @@ use Cake\ORM\TableRegistry;
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-<div class="row">
+<div>
     <div class="column-responsive column-80">
-        <div class="pokemons view content">
+        <div class="pokemons view content" style="margin-left:10%; margin-right:10%;">
             <h3 class="PokeName" align="center"><?= h($pokemon->name) ?></h3>
             <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="false">
-
                 <div class="carousel-inner">
-                    <div class="item">
-                        <img src="<?= h($pokemon->sprite_shiny) ?>" alt="Shiny" width="40%">
-                    </div>
-
-                    <div class="item active">
-                        <img src="<?= h($pokemon->default_front_sprite_url) ?>" alt="Default" width="40%">
-                    </div>
-
-                    <div class="item">
-                        <img src="<?= h($pokemon->sprite_back) ?>" alt="Back" width="40%">
-                    </div>
+                    <?php if(!empty($pokemon->sprite_shiny)){ ?>
+                        <div class="item" align="center">
+                            <img src="<?= h($pokemon->sprite_shiny) ?>" alt="Default" width="40%">
+                        </div>
+                    <?php } else{ ?>
+                        <div class="item" align="center" style="margin-top:15%; margin-bottom:20%">
+                            <img src="<?= h($pokemon->sprite_shiny) ?>" alt="Shiny" width="40%">
+                        </div>
+                    <?php } ?>
+                    <?php if(!empty($pokemon->default_front_sprite_url)){ ?>
+                        <div class="item active" align="center">
+                            <img src="<?= h($pokemon->default_front_sprite_url) ?>" alt="Default" width="40%">
+                        </div>
+                    <?php } else{ ?>
+                        <div class="item active" align="center" style="margin-top:15%; margin-bottom:20%">
+                            <img src="<?= h($pokemon->default_front_sprite_url) ?>" alt="Default" width="40%">
+                        </div>
+                    <?php } ?>
+                    <?php if(!empty($pokemon->sprite_back)){ ?>
+                        <div class="item" align="center">
+                            <img src="<?= h($pokemon->sprite_back) ?>" alt="Default" width="40%">
+                        </div>
+                    <?php } else{ ?>
+                        <div class="item" align="center" style="margin-top:15%; margin-bottom:20%">
+                            <img src="<?= h($pokemon->sprite_back) ?>" alt="Back" width="40%">
+                        </div>
+                    <?php } ?>
                 </div>
 
                 <a class="left carousel-control" href="#myCarousel" data-slide="prev" style="background:rgb(228, 228, 228);">
@@ -49,14 +64,11 @@ use Cake\ORM\TableRegistry;
                             <?php foreach ($pokemon->pokemon_stats as $pokemonStats) : ?>
                                 <tr>
                                     <?php
-                                        $query = TableRegistry::getTableLocator()->get('stats')->find();
-                                        $query
-                                        ->where(["id = $pokemonStats->stat_id"]);
-                                        foreach ($query as $article) { ?>
-                                            <td class="types"><?php echo $article->name;?></td>
+                                        $requete = TableRegistry::getTableLocator()->get('stats')->find()->where(["id = $pokemonStats->stat_id"]);
+                                        foreach ($requete as $stats) { ?>
+                                            <td class="types"><?php echo $stats->name;?></td>
                                             <td class="types"><?= h($pokemonStats->value) ?></td>
-                                            <?php } 
-                                    ?>
+                                    <?php }  ?>
                                 </tr>
                             <?php endforeach; ?>
                         </table>
